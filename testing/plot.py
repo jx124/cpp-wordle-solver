5747b84b59eac4ad4cb5e6529ae1d0fbcd78cf8f
jx124 <n.jiexiang@gmail.com>
2022-02-09 23:28:13 +0800

    Updated plotter and plots

--- testing/plot.py
+++ testing/plot.py
@@ -1,12 +1,12 @@
+from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Change path to absolute path
-random_data = np.genfromtxt(r"./testing/random_data.csv", delimiter = ',')
+random_data = np.genfromtxt(Path("./test_data/full_random_data.csv"), delimiter = ',')
 random_time = random_data[-1]
 random_data = random_data[:-1] 
 
-entropy_data = np.genfromtxt(r"./testing/entropy_data.csv", delimiter = ',')
+entropy_data = np.genfromtxt(Path("./test_data/full_entropy_data.csv"), delimiter = ',')
 entropy_time = entropy_data[-1]
 entropy_data = entropy_data[:-1] 
 
@@ -14,24 +14,24 @@ data = [random_data, entropy_data]
 labels = ["Random choice", "Entropy maximizing"]
 
 fig, ax = plt.subplots(figsize = (8,6))
-n_bins = 7 # 1 to 8
-plt.hist(data, bins = np.arange(1, n_bins + 3)-0.5, label = labels)
+n_bins = 11
+plt.hist(data, bins = np.arange(1, n_bins + 2)-0.5, label = labels)
 
-plt.title(f"Wordle Solver Results, n = {len(random_data)}", fontsize = 15)
+plt.title(f"Wordle Solver Results, Starting Word \"slate\", n = {len(random_data)}", fontsize = 15)
 plt.xlabel("Attempts", fontsize = 15)
 plt.ylabel("Frequency", fontsize = 15)
-plt.xticks(range(1, n_bins + 2))
+plt.xticks(range(1, n_bins + 1))
 plt.legend()
 
 random_mean = np.mean(random_data)
 plt.axvline(random_mean, c = 'k', ls = '--', lw = 1)
-plt.text(random_mean + 0.3, 1100, f"Random mean: {random_mean:.3f}\nElapsed time: {random_time} s", 
+plt.text(random_mean + 0.3, 3500, f"Random mean: {random_mean:.3f}\nElapsed time: {random_time} s", 
          transform = ax.transData, fontsize =11)
 
 entropy_mean = np.mean(entropy_data)
 plt.axvline(entropy_mean, c = 'k', ls = '--', lw = 1)
-plt.text(entropy_mean - 2.25, 1100, f"Entropy mean: {entropy_mean:.3f}\nElapsed time: {entropy_time} s", 
+plt.text(entropy_mean - 3.7, 3500, f"Entropy mean: {entropy_mean:.3f}\nElapsed time: {entropy_time} s", 
          transform = ax.transData, fontsize =11)
 
 plt.tight_layout()
-plt.savefig(r"./testing/plot.png")
+plt.savefig(Path("./full_plot.png"))
