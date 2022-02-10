400cc9c27ab670622c85273c395b58f1b046e7e8
jx124 <n.jiexiang@gmail.com>
2022-02-10 18:14:17 +0800

    Fixed error

--- testing/tester.cpp
+++ testing/tester.cpp
@@ -1,9 +1,5 @@
 #include "../wordle-solver.h"
 
-std::string randomly_choose_word(const std::vector<std::string>& words) {
-    return words[std::rand() % words.size()];
-}
-
 // Tests a given strategy of picking words and creates a csv file with all the attempts + elapsed time.
 // \param start_word std::string of length 5 of the initial guess.
 // \param words reference to std::vector of std::string containing all possible words.
@@ -11,42 +7,50 @@ std::string randomly_choose_word(const std::vector<std::string>& words) {
 // \param test_func function pointer to test function.
 void test_strategy(const std::string& start_word, const std::vector<std::string>& words, 
                    const std::string& outfile_name, std::string (*test_func)(const std::vector<std::string>&)) {
-    std::vector<int> attempts;
     std::string word, guess, result;
     std::vector<int> char_count(26,0);
+    std::ofstream data(outfile_name);
+
     auto start_time = std::chrono::steady_clock::now();
-
-    for (size_t n = 0; n < words.size(); n++) {
-        std::vector<std::string> temp_list = words; 
-        guess = start_word;
-        word = words[n];
+    for (size_t n = 0; n < words.size(); n++) { 
         int tries = 1;
+        auto word_list_copy = words;
+        auto test_word = words[n];
+        guess = start_word;
+        // data << guess;
 
-        if (n % 20 == 0) {
-            std::cout << "\rTrying " << word << ", word " << n << '/' << words.size();
+        if (n%100 == 0) {
+            std::cout << "\rTesting word " << n << '/' << words.size() << ": " << test_word << "     ";
         }
 
         while (true) {
-            result = evaluate(guess, word, char_count);
-            prune_word_list(guess, result, temp_list); 
-            if (temp_list.size() == 1) {
-                attempts.push_back(tries);
+            result = evaluate(guess, test_word, char_count);
+
+            if (result == "ggggg") {
+                data << tries << ',';
+                // data << ' ' << tries << '\n';
                 break;
             }
-            tries += 1;
-            guess = test_func(temp_list);
+
+            prune_word_list(guess, result, word_list_copy); 
+
+            if (word_list_copy.size() == 1) {
+                data << ++tries << ',';
+                // data << ',' << word_list_copy[0] << ' ' << ++tries << '\n';
+                break;
+            }
+            
+            guess = test_func(word_list_copy);
+            // data << ',' << guess;
+            ++tries;
         }
     }
 
     auto stop_time = std::chrono::steady_clock::now();
     auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(stop_time - start_time).count();
     auto seconds = float(elapsed_time) / 1000.0;
-
-    std::ofstream data(outfile_name);
-    for (auto attempt : attempts) {
-        data << attempt << ',';
-    }
     data << seconds;
+    std::cout << "\nElapsed time: " << seconds << " s\n";
 }
 
 
@@ -70,8 +74,8 @@ int main(int argc, const char** argv) {
         words.push_back(word); 
     }
 
-    test_strategy("slate", words, "random_data.csv", randomly_choose_word);
-    test_strategy("slate", words, "entropy_data.csv", find_max_entropy);
+    test_strategy("slate", words, "./test_data/random_data.csv", randomly_choose_word);
+    test_strategy("slate", words, "./test_data/entropy_data.csv", find_max_entropy);
 
     return 0;
 }
