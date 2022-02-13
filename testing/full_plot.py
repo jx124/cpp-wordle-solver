51b4a73ac78d6aafb29e1e7cac37f79804d69c8a
jx124 <n.jiexiang@gmail.com>
2022-02-14 01:11:15 +0800

    Updated plots, plotters, and data

--- testing/full_plot.py
+++ testing/full_plot.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
-random_data = np.genfromtxt(Path(__file__).parent.joinpath("test_data", "random_data.csv"), delimiter = ',')
+random_data = np.genfromtxt(Path(__file__).parent.joinpath("test_data", "full_random_data.csv"), delimiter = ',')
 random_time = random_data[-1]
 random_data = random_data[:-1] 
 
-entropy_data = np.genfromtxt(Path(__file__).parent.joinpath("test_data", "entropy_data.csv"), delimiter = ',')
+entropy_data = np.genfromtxt(Path(__file__).parent.joinpath("test_data", "full_entropy_data.csv"), delimiter = ',')
 entropy_time = entropy_data[-1]
 entropy_data = entropy_data[:-1] 
 
@@ -14,7 +14,7 @@ data = [random_data, entropy_data]
 labels = ["Random choice", "Entropy maximizing"]
 
 fig, ax = plt.subplots(figsize = (8,6))
-n_bins = 8
+n_bins = 10
 plt.hist(data, bins = np.arange(1, n_bins + 2)-0.5, label = labels)
 
 plt.title(f"Wordle Solver Results, Starting Word \"SLATE\", Hard Mode, n = {len(random_data)}", fontsize = 15)
@@ -26,7 +26,7 @@ plt.legend()
 random_mean = np.mean(random_data)
 plt.axvline(random_mean, c = 'k', ls = '--', lw = 1)
 plt.annotate(f"Random mean: {random_mean:.3f}\nElapsed time: {random_time} s",
-            (random_mean, 800),
+            (random_mean, 4000),
             xytext = (40, 0),
             textcoords="offset points",
             arrowprops = dict(arrowstyle="->", connectionstyle="arc3,rad=0.2", relpos = (0,0.5)),
@@ -35,19 +35,19 @@ plt.annotate(f"Random mean: {random_mean:.3f}\nElapsed time: {random_time} s",
 entropy_mean = np.mean(entropy_data)
 plt.axvline(entropy_mean, c = 'k', ls = '--', lw = 1)
 plt.annotate(f"Entropy mean: {entropy_mean:.3f}\nElapsed time: {entropy_time} s",
-            (entropy_mean, 900),
-            xytext = (-167, 0),
+            (entropy_mean, 4500),
+            xytext = (-172, 0),
             textcoords="offset points",
             arrowprops = dict(arrowstyle="->", connectionstyle="arc3,rad=-0.2", relpos = (1,0.5)),
             fontsize = 11)
 
-plt.axvline(3.421, c = 'b', ls = '--', lw = 1)
-plt.annotate("Optimal mean: 3.421\nElapsed time: 4.5 s",
-            (3.421, 800),
+plt.axvline(4.231, c = 'b', ls = '--', lw = 1)
+plt.annotate("Optimal mean: 4.231\nElapsed time: 50 h",
+            (4.231, 4000),
             xytext = (-160, 0),
             textcoords="offset points",
             arrowprops = dict(arrowstyle="->", connectionstyle="arc3,rad=-0.2", relpos = (1,0.5)),
             fontsize = 11)
 
 plt.tight_layout()
-plt.savefig(Path(__file__).parent.joinpath("plot.png"))
+plt.savefig(Path(__file__).parent.joinpath("full_plot.png"))
